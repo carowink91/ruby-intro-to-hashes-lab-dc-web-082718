0841179f3c5c4ea616ceac1f64d0f45b8346d9cc
angelica winkler <caw400@nyu.edu>
2018-08-04 02:02:17 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {}
-	monopoly.merge!(:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {})
+	monopoly.merge!(:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}})
 
 end
 
